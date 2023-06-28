3a4b761a116e044e2b84285060a8e60f44f5cbfa
aa-r-ju <aarju3447@gmail.com>
2023-06-28 21:07:26 +0545

    committed max-of-three.js

--- pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function lastCharacter(name1,name2) {
+ if(name1[name1.length -1] === name2[name2.length -1]) {
+    return true;
+} else {
+    return false;
+}
+}
+ console.log(lastCharacter('apples', 'pumpkins')); // => true
+
+console.log(lastCharacter('marker', 'pen')); // => false
--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function maxOfThree(val1,val2,val3) {
+    if(val1 > val2 && val1 > val3 ) {
+        return val1;
+    }
+    else if (val2 > val1 && val2 > val3 ) {
+        return val2;
+    }
+    else {
+         return val3;
+}
+
+}
+
+console.log(maxOfThree(5, 4, 10)); // => 10
+
+console.log(maxOfThree(7, 7, 4)); // => 7
