f327a44012f6992540606edff9a1b118f7d25331
aa-r-ju <aarju3447@gmail.com>
2023-06-28 20:24:39 +0545

    added greeting.js

--- pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function greeting(name) {
+    if(name) {
+        return  "Hello" + " " + name + "!";
+    } 
+    else {
+  return "Hello" + "!"
+}
+}
+//greeting("Sadie")
+greeting()
+greeting("Layla")
