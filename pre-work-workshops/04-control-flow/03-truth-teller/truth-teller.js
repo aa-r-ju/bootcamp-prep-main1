05b592aa8425dd7d1a7c2fa383c53e3209a53430
aa-r-ju <aarju3447@gmail.com>
2023-06-28 11:15:47 +0545

    cpmmitted truth-teller

--- pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -3,6 +3,13 @@ let boolean1 = true, boolean2 = true;
 // let boolean1 = false, boolean2 = true;
 // let boolean1 = false, boolean2 = false;
 
-let result;
+let result = "";
 
 // YOUR CODE BELOW
+if(boolean1 && boolean2 === true) {
+   result+= "both";
+} else if (boolean1 === true && boolean2 === false ) {
+   result+= "one";
+} else {
+    result+="none"
+}
