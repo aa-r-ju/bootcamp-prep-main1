3f37596ce0b18f891717a872008ae5a8a6c29c0e
aa-r-ju <aarju3447@gmail.com>
2023-06-28 10:30:04 +0545

    commited founder-finder.js

--- pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,8 @@
-let name = 'David';
-// let name = 'Nimit';
-// let name = 'Someone else';
-
-let found;
-
-// YOUR CODE BELOW
+let name = "David";
+//let name = "Nimit"
+//let name = "Someone else"
+if (name === "David" || "Nimit") {
+found =true;
+} else {
+ found = false;
+}
