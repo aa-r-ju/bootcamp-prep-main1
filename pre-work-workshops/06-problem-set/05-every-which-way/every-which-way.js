5ec737146b803c0080f05d0c2295b47ec4798bc8
aa-r-ju <aarju3447@gmail.com>
2023-06-28 21:18:49 +0545

    commiyed every-which-way

--- pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,28 @@
 // YOUR CODE BELOW
+function everyWhichWay(a,b,c){
+    if(a+b === c) {
+        return "sum";
+    } else if (a - b === c) {
+        return "difference";
+    } else if ((a === b) === c) {
+        return "square";
+    } else if(a / b === c ) {
+        return "fraction";
+    }  else if (a * b === c ) {
+        return "product"
+    }
+    else { 
+        return null
+    }
+}
+
+everyWhichWay(10, 20, 30); // => sum
+
+everyWhichWay(50, 20, 30); // => difference
+
+everyWhichWay(4, 4, 16); // => product
+
+everyWhichWay(100, 10, 10); // => fraction
+
+everyWhichWay(1, 1000, 50); // => null
+everyWhichWay(10, 5, 50)
