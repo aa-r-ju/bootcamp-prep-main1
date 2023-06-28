74e968df0622ea8d9d01ec0c32510db2524c5bc6
aa-r-ju <aarju3447@gmail.com>
2023-06-28 12:01:33 +0545

    committed tax-calculator.js

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function taxCalculator(price,state) {
+   if (state === "NY") {
+    return 4/100 * price + price 
+   } else {
+    return 6.625/100 * price + price 
+   }
+}
+
+console.log(taxCalculator(100,"NY"));
+taxCalculator(100,"NJ");
