353d147f8399df1f072d0f3763d62c661319e3a5
aa-r-ju <aarju3447@gmail.com>
2023-06-28 14:08:08 +0545

    committed my-mnemonic.js

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,20 @@
-// YOUR CODE BELOW
+function myMnemonic(a,b,c,d) {
+   let name = "";
+   if(a !== undefined) {
+    name+=a[0];
+   } 
+   if (b !== undefined) {
+    name+=b[0]
+   }
+   if(c !== undefined) {
+    name+=c[0];
+   }
+   if (d !== undefined) {
+    name+=d[0]
+   };
+   return name;
+}
+console.log(myMnemonic('Must', 'Accept', 'Sugar', 'Happiness')); // => MASH
+console.log(myMnemonic('We', 'Eat', 'Eggs')); // => WEE
+console.log(myMnemonic('Frankly', 'Umbrellas', 'Love', 'Leather'))
+
