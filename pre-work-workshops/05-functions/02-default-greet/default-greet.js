0f5387a8b422c414b9993b601bf19e8f3518dd70
aa-r-ju <aarju3447@gmail.com>
2023-06-28 11:34:52 +0545

    committed default-greet.js

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function defaultGreet(fname, lname="Doe") {
+   return "Hi " + fname + " " + lname + "!"
+}
+//defaultGreet('Chaka', 'Khan')
+//defaultGreet('John')
+//defaultGreet('John')
+defaultGreet('Natalie' , 'Cole')
+
