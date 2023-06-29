61c1f497d84e10b478ac7219147c952ef4685343
aa-r-ju <aarju3447@gmail.com>
2023-06-29 15:46:49 +0545

    committed only-odds.js

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,12 @@
-// YOUR CODE BELOW
+function onlyOdds(number) {
+    let val = 0;
+    for(let i = 0; i <= number; i++ ) {
+     if(i % 2 !== 0) {
+        val+=i;
+     }
+    }
+    return val
+} 
+
+console.log(onlyOdds(6));
+console.log(onlyOdds(10)); 
