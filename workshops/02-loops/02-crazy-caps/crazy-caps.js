48ae5110d0bbd1910af1d604412a17657e0ff898
aa-r-ju <aarju3447@gmail.com>
2023-06-30 08:00:23 +0545

    committed crazy-caps.js

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function crazyCaps(name) {
+    let value = "";
+    for( let i = 0; i < name.length; i++) {
+        if(i % 2 === 0 ) {
+            value+=name[i].toLowerCase();
+        } else {
+            value+=name[i].toUpperCase()
+        }
+    }
+     return value;
+}
+
+console.log(crazyCaps("this is crazy"));
